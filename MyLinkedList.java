2b8be0e7d373f03d724731a99dac9e41327c2f53
Jeff Weeks <jeffweeksio@users.noreply.github.com>
2015-03-27 11:49:36 -0400

    Create MyLinkedList.java

---
+++ MyLinkedList.java
@@ -0,0 +1,17 @@
+/*
+Jeff Weeks
+CSC 172: The Science of Data Structures
+University of Rochester
+Assignment: Project 3 (Word Search)
+3/31/14 
+*/
+
+public interface MyLinkedList {
+	
+	public void insert( Object x );
+	public void delete( Object x );
+	public boolean lookup( Object x );
+	public boolean isEmpty();
+	public void printList();
+	
+}
